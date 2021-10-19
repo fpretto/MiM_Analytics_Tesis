b049710b047217782d0c64cc15ee90e747cc3c1c
Fabricio Pretto <prettof@sancristobal.com.ar>
2021-10-19 08:08:34 +0200

    .

--- Tesis/queries/tesis_lk_leagues.sql
+++ Tesis/queries/tesis_lk_leagues.sql
@@ -12,5 +12,5 @@ CREATE TABLE fdm.dash_lk_leagues AS (
 SELECT A.* FROM fdm.tesis_lk_leagues_completeness AS A
 LEFT JOIN fdm.lk_api_countries AS B
 	ON A.country_name=B.country_name
-WHERE A.league_type='League' AND stats_players_pct>70 AND league_name!='Serie B' AND B.continent_code IN ('NA', 'SA')
+WHERE A.league_type='League' AND stats_players_pct>70 AND league_name!='Serie B' --AND B.continent_code IN ('NA', 'SA')
 );
